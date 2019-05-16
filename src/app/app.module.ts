869cc4ee4e95fdd0dc3a2e27b4ebec1bf6cead1f
FahadH <fahadh@logic2020.com>
2019-05-16 13:18:43 -0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ProductListComponent} from './Products/product-list.component';
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent], //root App component, belongs to root module
+  imports: [BrowserModule], //external module that belong to this module
+  bootstrap: [AppComponent] //startup component of Application, it should contain the selector we use
+})
+export class AppModule { } //identify the ng module by attaching a ngModule decorator
